e36015f0e54f8237c7454ba43f278bd4c6ffc239
youssef <64253722+obito@users.noreply.github.com>
2025-08-11 13:03:40 +0200

    add cloudflare pagesé

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     syntaxHighlight: {
       type: "shiki",
       excludeLangs: ["mermaid", "math"],
     },
   },
+
+  adapter: cloudflare(),
 });
